e7f5bbca1be2abceeb78f7562c30249765d90417
Andrey <6198760@gmail.com>
2022-11-26 23:19:04 +0300

    mod zadacha 4

--- zadacha_4/Program.cs
+++ zadacha_4/Program.cs
@@ -12,15 +12,22 @@ int number2 = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число: ");            //просим ввести третье число
 int number3 = int.Parse(Console.ReadLine());
 
-if (number1 > number2 && number1>number3)
-{
-        Console.WriteLine($"Максимальное число {number1}");
-    }
-if (number2 > number1 && number2>number3)
-{
-        Console.WriteLine($"Максимальное число {number2}");
-    }
-if (number3 > number1 && number3>number2)
-{
-        Console.WriteLine($"Максимальное число {number3}");
-    }
+int[] arr = {number1, number2, number3};
+
+Console.WriteLine("Максимальное число: " + arr.Max());
+
+
+
+//Метод номер № 1
+// if (number1 > number2 && number1>number3)
+// {
+//         Console.WriteLine($"Максимальное число {number1}");
+//     }
+// if (number2 > number1 && number2>number3)
+// {
+//         Console.WriteLine($"Максимальное число {number2}");
+//     }
+// if (number3 > number1 && number3>number2)
+// {
+//         Console.WriteLine($"Максимальное число {number3}");
+//     }
